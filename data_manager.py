728723d590eedb4666f4bc9d116b369fa75d3d79
Nicolai Rendtslev <nre@caliodesign.com>
2018-02-16 13:26:51 +0100

    Changed the way data_manager reads files to read the full file as a string, and added sys.argv functionality to open chosen dir

--- data_manager.py
+++ data_manager.py
@@ -1,14 +1,29 @@
 import glob 
+import sys
 
-all_files = glob.glob("instaloader/#valby/*.txt")
-count = 0
-random_list = []
-print(len(all_files))
-for file in all_files:
-    with open(file) as f:
-        for line in f:
-            random_list.append(str(line))
+def read_data(tagname):
+    dir = "instaloader/#" + tagname + "/*.txt"
+    all_files = glob.glob(dir)
+    if len(all_files) == 0:
+        raise RuntimeError('#' + hashtag + ' does not exist!')
+    count = 0
+    data_list = []
+    print(len(all_files))
+    for file in all_files:
+        opened_file = open(file, 'r')
+        data = opened_file.read()
+        #data.strip('\n')
+        data_list.append(data)
+        opened_file.close()
 
+hashtag = sys.argv[1]
+print('Trying to read directory: #' +hashtag)
+read_data(hashtag)
 
+    # check whether or not a file is containing \n for newline.
+# newline_files = 0
+# for i in random_list:
+#     if '\n' in i and count < 1:
+#         newline_files += 1
 
-print(len(random_list))
+# print(newline_files)
