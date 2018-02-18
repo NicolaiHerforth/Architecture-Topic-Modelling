dd7ca8c4114dc4bdf4f7decac7114aa15aa59282
Nicolai Rendtslev <nre@caliodesign.com>
2018-02-18 17:53:03 +0100

    Now removes majority of emojis, and stores caption and hashtags in separate lists as one long string

--- data_manager.py
+++ data_manager.py
@@ -1,26 +1,54 @@
 import glob 
 import sys
+import re
 
 def read_data(tagname):
     # defining the directory we want to open
     dir = "instaloader/#" + tagname + "/*.txt"
-    #   listing all files in dir
+    # listing all files in dir
     all_files = glob.glob(dir)
     if len(all_files) == 0:
-        raise RuntimeError('#' + hashtag + ' does not exist!')
-    data_list = []
+        raise RuntimeError('#' + hashtag_name + ' directory does not exist!')
+    data_list, captions, hashtags = [], [], []
 
-    print('#' + hashtag + ' contains', len(all_files), 'files.')
-    # then open file by file and store that data, then close file.
+    print('#' + hashtag_name + ' contains', len(all_files), 'files.')
+
+    # open file by file and store that data, then close file.
     for file in all_files:
         opened_file = open(file, 'r')
         data = opened_file.read()
         data_list.append(data)
         opened_file.close()
+    
+    # reading caption by caption and recognizing each word in the capion. if the word contains # it goes into
+    # the hashtags, else it goes into captions
+    for i in data_list: 
+        for word in i.split(' '):
+            if '#' in word:
+                hashtags.append(word)
+            else:
+                captions.append(word)
+        
+    # create a caption and a hashtag text joined by all the words collected, removing \n and # in the respective texts.
+    caption_text = ' '.join(captions).replace('\n',' ')
+    hashtag_text = ' '.join(hashtags).replace('\n', ' ').replace('#', '')
 
-hashtag = sys.argv[1]
-print('Trying to read directory: #' +hashtag)
-read_data(hashtag)
+    # removing emojis from text
+    emoji_pattern = re.compile("["
+                           u"\U0001F600-\U0001F64F"  
+                           u"\U0001F300-\U0001F5FF"
+                           u"\U0001F680-\U0001F6FF"
+                           u"\U0001F1E0-\U0001F1FF"
+                           "]+", flags=re.UNICODE)
+    caption_text = emoji_pattern.sub(r'', caption_text)
+    hashtag_text = emoji_pattern.sub(r'', hashtag_text)
+    
+
+
+hashtag_name = sys.argv[1]
+print('Trying to read directory: #' +hashtag_name)
+read_data(hashtag_name)
+
 
     # check whether or not a file is containing \n for newline.
 # newline_files = 0
