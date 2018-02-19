241a361548476d2a78c172b00cf57c31030d8b67
Nicolai Rendtslev <nre@caliodesign.com>
2018-02-19 16:27:51 +0100

    added nørrebro and exported to a list with comme separated strings

--- data_manager.py
+++ data_manager.py
@@ -8,10 +8,10 @@ def read_data(tagname):
     # listing all files in dir
     all_files = glob.glob(dir)
     if len(all_files) == 0:
-        raise RuntimeError('#' + hashtag_name + ' directory does not exist!')
-    data_list, captions, hashtags = [], [], []
+        raise RuntimeError('#' + tagname + ' directory does not exist!')
+    data_list, captions, hashtags, full_post = [], [], [], []
 
-    print('#' + hashtag_name + ' contains', len(all_files), 'files.')
+    print('#' + tagname + ' contains', len(all_files), 'files.')
 
     # open file by file and store that data, then close file.
     for file in all_files:
@@ -19,29 +19,46 @@ def read_data(tagname):
         data = opened_file.read()
         data_list.append(data)
         opened_file.close()
-    
+
     # reading caption by caption and recognizing each word in the capion. if the word contains # it goes into
     # the hashtags, else it goes into captions
     for i in data_list: 
+        # creating an empty string for each post, ready to be filled with the content of the post
+        current_caption = ""
+        current_hashtags = ""
+        current_full_post = ""
+        
         for word in i.split(' '):
             if '#' in word:
-                hashtags.append(word)
+                # adding the hashtags from the current post, to the hashtag string
+                current_hashtags += word + ' '
             else:
-                captions.append(word)
-        
-    # create a caption and a hashtag text joined by all the words collected, removing \n and # in the respective texts.
-    caption_text = ' '.join(captions).replace('\n',' ')
-    hashtag_text = ' '.join(hashtags).replace('\n', ' ').replace('#', '')
+                # adding the caption text from the current post, to the caption string
+                current_caption += word + ' '
 
-    # removing emojis from text
-    emoji_pattern = re.compile("["
-                           u"\U0001F600-\U0001F64F"  
-                           u"\U0001F300-\U0001F5FF"
-                           u"\U0001F680-\U0001F6FF"
-                           u"\U0001F1E0-\U0001F1FF"
-                           "]+", flags=re.UNICODE)
-    caption_text = emoji_pattern.sub(r'', caption_text)
-    hashtag_text = emoji_pattern.sub(r'', hashtag_text)
+            # removing emojis from text
+        emoji_pattern = re.compile("["
+                            u"\U0001F600-\U0001F64F"
+                            u"\U0001F300-\U0001F5FF"
+                            u"\U0001F680-\U0001F6FF"
+                            u"\U0001F1E0-\U0001F1FF"
+                            "]+", flags=re.UNICODE)
+        current_caption = emoji_pattern.sub(r'', current_caption)
+        current_hashtags = emoji_pattern.sub(r'', current_hashtags)
+
+        #remove \n and #
+        
+        current_caption = current_caption.replace('\n',' ')
+        current_hashtags = current_hashtags.replace('\n',' ')
+        current_full_post += current_caption + current_hashtags
+
+        current_hashtags = current_hashtags.replace('#','')
+        # append the original lists with the string we just created.
+        hashtags.append(current_hashtags)
+        captions.append(current_caption)
+        full_post.append(current_full_post)
+
+    
     
 
 
