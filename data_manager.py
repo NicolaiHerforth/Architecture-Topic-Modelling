b920cbafd8dac58d0e8d07125ebf5b4965af1f66
Therese Moreau <tmh@Thereses-MacBook-Pro.local>
2018-02-19 16:35:06 +0100

    Tilføje NME, printer nu 20 topics

--- data_manager.py
+++ data_manager.py
@@ -1,6 +1,14 @@
 import glob 
 import sys
 import re
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.decomposition import NMF, LatentDirichletAllocation
+
+def display_topics(model, feature_names, no_top_words):
+    for topic_idx, topic in enumerate(model.components_):
+        print('Topic %d:' % (topic_idx))
+        print(' '.join([feature_names[i]
+            for i in topic.argsort()[:-no_top_words - 1:-1]]))
 
 def read_data(tagname):
     # defining the directory we want to open
@@ -57,13 +65,19 @@ def read_data(tagname):
         hashtags.append(current_hashtags)
         captions.append(current_caption)
         full_post.append(current_full_post)
-
+        
+        no_topics = 20
+        tfidf_vectorizer = TfidfVectorizer(max_df = 0.95, min_df = 2, max_features = None, stop_words = 'english')
+        tfidf = tfidf_vectorizer.fit_transform(data_list)
+        tfidf_feature_names = tfidf_vectorizer.get_feature_names()
     
-    
+        nmf = NMF(n_components = no_topics, random_state = 1, alpha = .1, l1_ratio = .5, init = 'nndsvd').fit(tfidf)
+        no_top_words = 10
+        display_topics(nmf, tfidf_feature_names, no_top_words)
 
 
 hashtag_name = sys.argv[1]
-print('Trying to read directory: #' +hashtag_name)
+print('Trying to read directory: #' + hashtag_name)
 read_data(hashtag_name)
 
 
