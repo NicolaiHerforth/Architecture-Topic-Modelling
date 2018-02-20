15800d9e7d8a7dd2e9a41d4f1c4fb5dd363a8827
David Mortensen <davidmortensen@Davids-MacBook-Pro-2.local>
2018-02-20 15:44:43 +0100

    start of method for dict

--- Analysis.py
+++ Analysis.py
@@ -1,10 +1,27 @@
 def import_data(location):
-    dir = "instaloader/#" + location + "/*.txt"
+    dir_cap = "data/" + location + "/captions.txt"
+    dir_full = "data/" + location + "/full_post.txt"
+    dir_hash = "data/" + location + "/hashtags.txt"
+    id = 0
 
-    with open("hashtags.txt") as file:
-        i = 0
-        for line in file:
-            i += 1
-            d[key[i]] = val[line]
+    d = {}
+    with open(dir_full, 'r') as a, open(dir_cap, 'r') as b, open(dir_hash, 'r') as c:
+        
+        for line_a, line_b, line_c in zip(a, b, c):
+            #print(line_a, line_b, line_c)
+            line_a = line_a.strip('\n')
+            line_b = line_b.strip('\n')
+            line_c = line_c.strip('\n')
+            d[id] = list()
+            d[id].extend((line_a,line_b,line_c))
+            id += 1
 
-import_data(valby)
+    
+    #skal altid stå nederst
+    for key in d.keys():
+        pass
+       
+    
+
+import_data('valby')
+
