4bc74c079375b8418e207d48c38475a2d8809432
David Mortensen <davidmortensen@Davids-MacBook-Pro-2.local>
2018-02-20 19:04:54 +0100

    Added exporting to .csv with respective values in their own row.

--- Analysis.py
+++ Analysis.py
@@ -1,5 +1,9 @@
 from nltk import wordpunct_tokenize
 from nltk.corpus import stopwords
+from afinn import Afinn
+import csv
+
+
 
 #Insert language
 def get_language(caption):
@@ -22,6 +26,22 @@ def get_language(caption):
         
     return most_rated_language
 
+def sentiment(sentence):
+    """
+    Requires afinn: "pip install afinn"
+    Uses Danish made Afinn module to perform sentiment analysis on a word or sentence.
+    Returns sentiment label: Positive, Neutral or Negative for given input.
+    """
+    afinn = Afinn(language = 'da')
+
+    if afinn.score(sentence) > 0:
+        return "Positive"
+    elif afinn.score(sentence) == 0:
+        return "Neutral"
+    else:
+        return "Negative"
+
+
 def import_data(locations):
     id = 0
     d = {}
@@ -31,20 +51,24 @@ def import_data(locations):
         dir_hash = "data/" + location + "/hashtags.txt"
         
         with open(dir_full, 'r') as a, open(dir_cap, 'r') as b, open(dir_hash, 'r') as c:
-            
             for line_a, line_b, line_c in zip(a, b, c):
                 #print(line_a, line_b, line_c)
                 line_a = line_a.strip('\n').replace('#','')
                 line_b = line_b.strip('\n')
                 line_c = line_c.strip('\n')
                 lang = get_language(line_a)
+                senti = sentiment(line_a)
                 d[id] = list()
-                d[id].extend((line_a,line_b,line_c, lang, location))
+                d[id].extend((line_a,line_b,line_c, lang, location, senti))
                 id += 1
-        
-    #skal altid stå nederst
-    for key in d.keys():
-        print(d[key][0],d[key][3], d[key][4])
 
-locations = ['valby', 'nørrebro']    
+                    
+
+    with open('dict.csv', 'w') as csv_file:
+        writer = csv.writer(csv_file)
+        
+        for key, value in d.items():
+            writer.writerow([key,value[0],value[1],value[2],value[3],value[4],value[5]])
+
+locations = ['valby']    
 import_data(locations)
