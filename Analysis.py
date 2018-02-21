07bf6e8f96dfbdfbd2ab8e92fd10e5ecc7a77b20
calior <nre@caliodesign.com>
2018-02-21 12:42:14 +0100

    Added all scraped files and did data management and exported final dict to dict.csv

--- Analysis.py
+++ Analysis.py
@@ -12,18 +12,18 @@ def get_language(caption):
     words = [word.lower() for word in words]
 
     languages_dict = {}
-    
+
     for language in stopwords.fileids():
         stopwords_set = set(stopwords.words(language))
         words_set = set(words)
         common_elements = words_set.intersection(stopwords_set)
-    
+
         languages_dict[language] = len(common_elements) # language "score"
-        
+
     most_rated_language = str(max(languages_dict, key = languages_dict.get))
     if most_rated_language != 'english' and 'danish':
         most_rated_language = 'Unknown'
-        
+
     return most_rated_language
 
 def sentiment(sentence):
@@ -49,7 +49,7 @@ def import_data(locations):
         dir_cap = "data/" + location + "/captions.txt"
         dir_full = "data/" + location + "/full_post.txt"
         dir_hash = "data/" + location + "/hashtags.txt"
-        
+        print('Currently working ', location)
         with open(dir_full, 'r') as a, open(dir_cap, 'r') as b, open(dir_hash, 'r') as c:
             for line_a, line_b, line_c in zip(a, b, c):
                 #print(line_a, line_b, line_c)
@@ -62,13 +62,13 @@ def import_data(locations):
                 d[id].extend((line_a,line_b,line_c, lang, location, senti))
                 id += 1
 
-                    
+
 
     with open('dict.csv', 'w') as csv_file:
         writer = csv.writer(csv_file)
-        
+
         for key, value in d.items():
             writer.writerow([key,value[0],value[1],value[2],value[3],value[4],value[5]])
-
-locations = ['valby']    
+    print('JOBS DONE!')
+locations = ['valby', 'nørrebro', 'vesterbro', 'amager', 'torvehallerne', 'indreby', 'østerbro']
 import_data(locations)
