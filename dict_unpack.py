6dc4419671185e1a9c1ff87ffe4b430a3d37b7de
Therese Moreau <tmh@Thereses-MacBook-Pro.local>
2018-02-23 16:13:04 +0100

    Dict_unpack as method

--- dict_unpack.py
+++ dict_unpack.py
@@ -1,8 +1,25 @@
-with open('dict.csv', 'r') as file:
-    rows = (line.split('\t') for line in file)
-    d = {int(row[0]) : row[1:] for row in rows}
-    
-for row in d:
-    d[row][5] = d[row][5][:-1]
+import sys
 
-print(d[0])
+def unpack(dictionary):
+    """
+    Unpacks dict from csv file. The dictionary will have the following indexing parameters:
+    key = unique id for entry
+    val0 = full text
+    val1 = caption
+    val2 = hashtags
+    val3 = language (DA, EN, unknown)
+    val4 = location
+    val5 = sentiment label (pos, neg, neu)
+    """
+
+    with open(dictionary, 'r') as file:
+        rows = (line.split('\t') for line in file)
+        d = {int(row[0]) : row[1:] for row in rows}
+
+    #Remove \n in column 5    
+    for row in d:
+        d[row][5] = d[row][5][:-1]
+
+dictionary = sys.argv[1]
+print('Unpacking dictionary: ' + dictionary)
+unpack(dictionary)
